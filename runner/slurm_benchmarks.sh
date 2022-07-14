eb7be2094d829fce89d9dda6b9734b8372833bfe
swryan <swryan@users.noreply.github.com>
2022-07-14 18:00:01 -0400

    add timeout to slurm job

--- runner/slurm_benchmarks.sh
+++ runner/slurm_benchmarks.sh
@@ -49,7 +49,7 @@ cat << EOM >$RUN_NAME.sh
 export OMPI_MCA_mpi_warn_on_fork=0
 ulimit -s 10240
 
-testflo -n 1 -bvs -o $OUT_FILE -d $CSV_FILE
+testflo -n 1 --timeout=300 -bvs -o $OUT_FILE -d $CSV_FILE
 EOM
 
 # submit the job
