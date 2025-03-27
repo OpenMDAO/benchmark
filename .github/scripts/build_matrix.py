4b112f2dea29151a51c26eba5a18a7a7e4e8b1c0
swryan <swryan@users.noreply.github.com>
2025-03-27 15:46:09 -0400

    nojson

--- .github/scripts/build_matrix.py
+++ .github/scripts/build_matrix.py
@@ -6,7 +6,7 @@ matrix = {'include': []}
 
 if len(sys.argv) == 1 or (len(sys.argv) == 2 and sys.argv[1] == ''):
     files = os.listdir('.')
-    projects = [f for f in files if f.endswith('.json')]
+    projects = [f[:-5] for f in files if f.endswith('.json')]
 else:
     projects = [p for p in sys.argv[1:]]
 
