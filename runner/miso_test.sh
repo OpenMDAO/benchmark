66e8e876c4b40562e60bae6e1b99f69d53b0d7c2
swryan <swryan@users.noreply.github.com>
2025-04-08 13:06:34 -0400

    libopenmpi-dev

--- runner/miso_test.sh
+++ runner/miso_test.sh
@@ -23,6 +23,8 @@ if command -v module &> /dev/null; then
 
   export MPICC=/cryo/sw/openmpi/4.1.4/gnu/11.2.0/bin/mpicc
   export MPICXX=/cryo/sw/openmpi/4.1.4/gnu/11.2.0/bin/mpicxx
+else
+  sudo apt install libopenmpi-dev
 fi
 
 export OMPI_MCA_rmaps_base_oversubscribe=1
@@ -43,7 +45,7 @@ if ! conda env list | grep miso_test; then
   conda create --yes -n miso_test python=3.11 cython swig metis
   conda activate miso_test
   export METIS_DIR=$CONDA_PREFIX
-  pip install openmpi-mpicc mpi4py mkdocs
+  pip install mpi4py mkdocs
 else
   conda activate miso_test
 fi
